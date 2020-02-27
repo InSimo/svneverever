341ce73ec58afe7dbd0f0b4fdfff225a6b5480fb
Jeremie Allard <jeremie.allard@insimo.fr>
2020-02-27 19:18:22 +0100

    Add --externals to dump urls and revisions / date ranges referenced from svn:externals properties
    This is useful to know which other repository should be converted in order to preserve the full history.

--- modules/svneverever/__main__.py
+++ modules/svneverever/__main__.py
@@ -7,9 +7,11 @@
 #
 from __future__ import print_function
 
+import datetime
 import getpass
 import math
 import os
+import shlex
 import signal
 import sys
 import time
@@ -19,6 +21,7 @@ import pysvn
 import six
 from six.moves import xrange
 from six.moves.urllib.parse import quote as urllib_parse_quote
+from six.moves.urllib.parse import urljoin as urllib_parse_urljoin
 
 try:
     import argparse
@@ -145,6 +148,51 @@ def dump_nick_stats(nick_stats, revision_digits, config):
             print(_for_print(nick))
 
 
+
+def dump_externals(externals, revision_digits, latest_revision, latest_date,
+    config):
+    last_path = []
+    base_ident = 1 + revision_digits + 2 + revision_digits + 1 + 3 \
+        if config.show_numbers else 0
+    print('\nEXTERNALS\n')
+    for ext_dir, ext_items in sorted(externals.items()):
+        path = ext_dir.split('/')
+        if config.flat_tree:
+            prefix = ext_dir
+        else:
+            for level, name in enumerate(path):
+                if level >= len(last_path) or last_path[level] != name:
+                    print('{}{}'.format(' '*(base_ident + 4 * level),
+                        _for_print(name)))
+            prefix = ' '*(4*len(path)-2)
+        last_path = path
+        for (ext_url, ext_unpeg), (added_on_rev, last_deleted_on_rev, \
+                remote_min_rev, remote_max_rev) in ext_items.items():
+            if config.show_numbers:
+                format = '(%%%dd; %%%dd)  ' \
+                    % (revision_digits, revision_digits)
+                if last_deleted_on_rev is not None:
+                    last_seen_rev = last_deleted_on_rev - 1
+                else:
+                    last_seen_rev = latest_revision
+                visual_rev = format % (added_on_rev, last_seen_rev)
+            else:
+                visual_rev = ''
+            if ext_unpeg: #isinstance(remote_min_rev, datetime.datetime):
+                if last_deleted_on_rev is None:
+                    remote_max_rev = latest_date
+                visual_remote_rev = '({}; {})'.format(
+                    remote_min_rev.strftime('%Y-%m-%d'),
+                    remote_max_rev.strftime('%Y-%m-%d'))
+            else:
+                visual_remote_rev = '({}; {})'.format(
+                    remote_min_rev, remote_max_rev)
+            print('{}{} {} {}  {}'.format(
+                visual_rev, prefix,
+                ('=>' if ext_unpeg else '->'),
+                ext_url, visual_remote_rev))
+
+
 def ensure_uri(text):
     import re
     svn_uri_detector = re.compile('^[A-Za-z+]+://')
@@ -170,6 +218,77 @@ def hms(seconds):
     m = int(seconds / 60)
     seconds = seconds - m*60
     return h, m, seconds
+
+
+def externals_parse(exts, url_root='', dir_current=''):
+    exts_dict = dict() # dir -> (url, rev) where rev = None if not specified
+    if not exts:
+        return exts_dict
+    if dir_current and not dir_current.endswith('/'):
+        dir_current += '/'
+    if url_root and not url_root.endswith('/'):
+        url_root += '/'
+    url_current = url_root + dir_current
+    for l in exts.replace('\r\n', '\n').split('\n'):
+        tok = shlex.split(l.strip())
+        if len(tok) == 0:
+            continue
+        if '://' in tok[-1]:
+            # pre svn-1.5 format
+            ext_url = tok[-1]
+            ext_dir = tok[0]
+            ext_rev = None
+            if len(tok) > 2:
+                if tok[1].startswith('-r') and (
+                        (len(tok[1]) > 2 and len(tok) == 3) or
+                        (len(tok[1]) == 2 and len(tok) == 4) ):
+                    ext_rev = int(tok[1][2:] if len(tok[1]>2) else tok[2])
+                else:
+                    print('FAILED to parse svn:externals line "{}" (1)\n'
+                          .format(l), file=sys.stderr)
+                    continue
+        else:
+            ext_rev = None
+            if tok[0].startswith('-r'):
+                if len(tok[0]) > 2:
+                    ext_rev = int(tok[0][2:])
+                    tok = tok[1:]
+                elif len(tok) >= 4:
+                    ext_rev = int(tok[1])
+                    tok = tok[2:]
+                else:
+                    print('FAILED to parse svn:externals line "{}" (2)\n'
+                          .format(l), file=sys.stderr)
+                    continue
+            if tok[0].startswith('//') or tok[0].startswith('/') \
+                or tok[0].startswith('../'):
+                # standard relative urls to current url
+                ext_url = urllib_parse_urljoin(url_current, tok[0])
+                tok = tok[1:]
+            elif tok[0].startswith('^/'): # or tok[0].startswith('^
+                # urls relative to root url
+                ext_url = urllib_parse_urljoin(url_root, tok[0][2:])
+                tok = tok[1:]
+            elif '://' in tok[0]:
+                ext_url = tok[0]
+                tok = tok[1:]
+            else:
+                print('FAILED to parse svn:externals line "{}" (3)\n'
+                      .format(l),file=sys.stderr)
+                continue
+            if len(tok) > 1:
+                print('FAILED to parse svn:externals line "{}" (4)\n'
+                      .format(l), file=sys.stderr)
+                continue
+            ext_dir = tok[0]
+        if '@' in ext_url:
+            if ext_rev is None:
+                ext_rev = int(ext_url.split('@')[1])
+            ext_url = ext_url.split('@')[0]
+        exts_dict[dir_current + ext_dir] = (ext_url, ext_rev)
+    #print('PARSED from {} {} {}\n{}\n=> {}\n'
+    #      .format(url_root, url_current, dir_current, exts, exts_dict))
+    return exts_dict
 
 
 def make_progress_bar(percent, width, seconds_taken, seconds_expected):
@@ -208,6 +327,11 @@ def command_line():
         '--committers',
         dest='nick_stat_mode', action='store_true', default=False,
         help='Collect committer names instead of path information '
+             '(default: disabled)')
+    modes.add_argument(
+        '--externals',
+        dest='show_externals', action='store_true', default=False,
+        help='Collect svn:externals in addition to path information '
              '(default: disabled)')
 
     common = parser.add_argument_group('common arguments')
@@ -337,15 +461,21 @@ def main():
         sys.stderr.flush()
 
     nick_stats = dict()
+    externals_props = dict()
+    externals = dict()
 
     for rev in xrange(1, latest_revision + 1):
         if rev == 1 and args.show_progress:
             indicate_progress(rev, before_work=True)
 
+        last_rev_object \
+            = pysvn.Revision(pysvn.opt_revision_kind.number, rev - 1)
+        rev_object = pysvn.Revision(pysvn.opt_revision_kind.number, rev)
+           
         if args.nick_stat_mode:
             committer_name = client.revpropget(
                 'svn:author', args.repo_uri,
-                pysvn.Revision(pysvn.opt_revision_kind.number, rev))[1]
+                rev_object)[1]
             if not committer_name:
                 committer_name = args.unknown_committer_name
             (first_commit_rev, last_commit_rev, commit_count) \
@@ -365,12 +495,13 @@ def main():
 
         summary = client.diff_summarize(
             args.repo_uri,
-            revision1=pysvn.Revision(pysvn.opt_revision_kind.number, rev - 1),
+            revision1=last_rev_object,
             url_or_path2=args.repo_uri,
-            revision2=pysvn.Revision(pysvn.opt_revision_kind.number, rev),
+            revision2=rev_object,
             recurse=True,
             ignore_ancestry=True)
-
+        #print("\n".join([str([str(e)+"="+str(v) for e,v in vars(s).items()])
+        #  for s in summary]))
         def is_directory_addition(summary_entry):
             return (summary_entry.summarize_kind
                     == pysvn.diff_summarize_kind.added
@@ -379,6 +510,10 @@ def main():
         def is_directory_deletion(summary_entry):
             return (summary_entry.summarize_kind
                     == pysvn.diff_summarize_kind.delete
+                    and summary_entry.node_kind == pysvn.node_kind.dir)
+
+        def is_directory_properties(summary_entry):
+            return (summary_entry.prop_changed
                     and summary_entry.node_kind == pysvn.node_kind.dir)
 
         dirs_added = [e.path for e in summary if is_directory_addition(e)]
@@ -397,26 +532,86 @@ def main():
                         sub_tree[name] = (added_on_rev, None, children)
                 sub_tree = children
 
-        def mark_deleted_recursively(sub_tree, name, rev):
+        def mark_deleted_recursively(sub_tree, absname, name, rev, all_dirs):
             added_on_rev, last_deleted_on_rev, children = sub_tree[name]
+            all_dirs.append(absname)
             if last_deleted_on_rev is None:
                 sub_tree[name] = (added_on_rev, rev, children)
                 for child_name in children.keys():
-                    mark_deleted_recursively(children, child_name, rev)
+                    mark_deleted_recursively(children,
+                        absname + '/' + child_name, child_name, rev, all_dirs)
 
         dirs_deleted = [e.path for e in summary if is_directory_deletion(e)]
+        all_dirs_deleted = []
         for d in dirs_deleted:
             sub_tree = tree
             comps = d.split('/')
             comps_len = len(comps)
             for i, name in enumerate(comps):
                 if i == comps_len - 1:
-                    mark_deleted_recursively(sub_tree, name, rev)
+                    mark_deleted_recursively(sub_tree, d, name, rev,
+                                             all_dirs_deleted)
                 else:
                     added_on_rev, last_deleted_on_rev, children \
                         = sub_tree[name]
                     sub_tree = children
 
+        if args.show_externals:
+            dirs_props = [e.path for e in summary if is_directory_properties(e)]
+            commit_date = None # only get if needed (for unpeg externals)
+            for d in set(dirs_props) | set(dirs_added) | set(all_dirs_deleted):
+                if d in all_dirs_deleted:
+                    exts = {}
+                else:
+                    exts = client.propget(
+                        'svn:externals', args.repo_uri + '/' + d, rev_object,
+                        recurse=False)
+                    exts = list(exts.values())[0] if exts else ''
+                    exts = externals_parse(exts, args.repo_uri, d)
+                last_exts = externals_props.get(d,{})
+                externals_props[d] = exts
+                for ext_dir in set(last_exts) | set(exts):
+                    ext_url, ext_rev = exts.get(ext_dir, (None, None))
+                    last_ext_url, last_ext_rev \
+                        = last_exts.get(ext_dir, (None, None))
+                    ext_unpeg = bool(ext_url and not ext_rev)
+                    last_ext_unpeg = bool(last_ext_url and not last_ext_rev)
+                    if ext_unpeg or last_ext_unpeg:
+                        if not commit_date:
+                            commit_date = client.revpropget(
+                                'svn:date', args.repo_uri,
+                                rev_object)[1]
+                            commit_date = datetime.datetime.strptime(
+                                commit_date, "%Y-%m-%dT%H:%M:%S.%f%z")
+                        if ext_unpeg:
+                            ext_rev = commit_date
+                        if last_ext_unpeg:
+                            # remove one microsecond
+                            last_ext_rev = commit_date \
+                                - datetime.timedelta(microseconds=1)
+                    if ext_dir not in externals:
+                        externals[ext_dir] = dict()
+                    if last_ext_url and (ext_url != last_ext_url or \
+                        ext_unpeg != last_ext_unpeg):
+                        # mark last dir -> URL link as deleted
+                        first_commit_rev, last_deleted_on_rev, \
+                            remote_min_rev, remote_max_rev \
+                            = externals[ext_dir][(last_ext_url, last_ext_unpeg)]
+                        last_deleted_on_rev = rev
+                        externals[ext_dir][(last_ext_url, last_ext_unpeg)] \
+                            = ( first_commit_rev, last_deleted_on_rev, \
+                                min(remote_min_rev, last_ext_rev),
+                                max(remote_max_rev, last_ext_rev) )
+                    if ext_url: # add or update dir -> URL link
+                        first_commit_rev, last_deleted_on_rev, \
+                            remote_min_rev, remote_max_rev \
+                            = externals[ext_dir].get((ext_url, ext_unpeg),
+                            (rev, None, ext_rev, ext_rev))
+                        last_deleted_on_rev = None
+                        externals[ext_dir][(ext_url, ext_unpeg)] \
+                            = ( first_commit_rev, last_deleted_on_rev, \
+                                min(remote_min_rev, ext_rev),
+                                max(remote_max_rev, ext_rev) )
         if args.show_progress:
             indicate_progress(rev)
 
@@ -431,7 +626,16 @@ def main():
     else:
         dump_tree(tree, digit_count(latest_revision), latest_revision,
                   config=args)
-
+        if args.show_externals:
+            latest_date = client.revpropget(
+                'svn:date', args.repo_uri,
+                pysvn.Revision(pysvn.opt_revision_kind.number,
+                latest_revision))[1]
+            latest_date = datetime.datetime.strptime(
+                latest_date, "%Y-%m-%dT%H:%M:%S.%f%z") if latest_date \
+                else datetime.datetime.now()
+            dump_externals(externals, digit_count(latest_revision),
+                  latest_revision, latest_date, config=args)
 
 if __name__ == '__main__':
     main()
